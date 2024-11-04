053bea6b12b4fc371a3c3cbcec5f73ce931ed0c1
rl-carno <dev@carno.io>
2024-11-04 06:34:17 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'image-cropper',
+  webDir: 'www'
+};
+
+export default config;
